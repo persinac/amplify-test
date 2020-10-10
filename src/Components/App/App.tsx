ef20a5ac5fcd47ce55b6ef2c4dcfc7f8dcfd9156
Alex Persinger <apfbacc@gmail.com>
2020-10-10 18:11:49 -0400

    EJECTION. Let's see if this worked. Also switched to HashRouter vs BrowserRouter

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import * as firebase from "../../Firebase/firebase";
 import { Navigation } from '../Navigation/Navigation';
 import { SignUp } from '../SignUp';
